06113003a303de3007c0f568a75546a083082fd5
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-06-04 08:46:25 +0200

    refactor: separated query tests to own file

---
+++ apps/backend/src/db/queries/auth-queries.test.ts
@@ -0,0 +1,90 @@
+import { describe, expect, it, beforeEach, afterEach } from "bun:test";
+import { Database } from "bun:sqlite";
+import { createTestDb } from "../../test/test-db";
+
+import { getUserByEmail, getUserById, insertUser } from "./auth-queries";
+
+let db: Database;
+
+beforeEach(() => {
+  db = createTestDb();
+});
+
+afterEach(() => {
+  db.close();
+});
+
+
+
+describe("insertUser", () => {
+  it("inserts user", async () => {
+    const email = "test@gmail.com";
+    const password = "password123";
+    const userId = await insertUser(db, email, password);
+    expect(userId).toBeDefined();
+  });
+
+  it("throws error if the user email is already taken", async () => {
+    const email = "test@gmail.com";
+    const password = "password123";
+    await insertUser(db, email, password);
+    try {
+      await insertUser(db, email, password);
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(error.message).toMatch(/UNIQUE constraint failed/);
+      }
+    }
+  });
+  it("throws an error if password is empty", async () => {
+    const email = "testtest.com";
+    const password = "";
+    try {
+      await insertUser(db, email, password);
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(error.message).toMatch(/password must not be empty/);
+      }
+    }
+  });
+});
+
+describe("getUsersByEmail", () => {
+  it("returns user by email", async () => {
+    const email = "test@test.com";
+    const password = "password123";
+    await insertUser(db, email, password);
+    const user = getUserByEmail(db, email);
+    expect(user).toEqual({
+      id: expect.any(String),
+      password_hash: expect.any(String),
+    });
+  });
+
+  it("returns null if user does not exist", async () => {
+    const email = "test@test.com";
+    const user = getUserByEmail(db, email);
+    expect(user).toBeNull();
+  });
+});
+
+describe("getUsersById", () => {
+  it("returns user by id", async () => {
+    const email = "test@test.com";
+    const password = "password123";
+    const userId = await insertUser(db, email, password);
+    const user = getUserById(db, userId);
+    expect(user).toEqual({
+      id: expect.any(String),
+      email: "test@test.com",
+    });
+  });
+
+  it("returns null if user does not exist", async () => {
+    const user = getUserById(db, "anyId");
+    expect(user).toBeNull();
+  });
+});
+
--- apps/backend/src/db/queries/workout-queries.test.ts
+++ apps/backend/src/db/queries/workout-queries.test.ts
@@ -1,10 +1,15 @@
 import { describe, expect, it, beforeEach, afterEach } from "bun:test";
 import { Database } from "bun:sqlite";
-import { createTestDb } from "../test/test-db";
+import { createTestDb } from "../../test/test-db";
 
-import type { WorkoutData } from "../types/workout";
-import { getUserByEmail, getUserById, insertUser } from "./queries/auth-queries";
-import { deleteWorkoutById, getWorkoutById, getWorkoutsByUserId, insertWorkout, updateWorkoutById } from "./queries/workout-queries";
+import type { WorkoutData } from "../../types/workout";
+import {
+  deleteWorkoutById,
+  getWorkoutById,
+  getWorkoutsByUserId,
+  insertWorkout,
+  updateWorkoutById,
+} from "./workout-queries";
 
 let db: Database;
 
@@ -22,82 +27,10 @@ const workoutData: WorkoutData = {
   notes: "just a couple of notes",
 };
 
-describe("insertUser", () => {
-  it("inserts user", async () => {
-    const email = "test@gmail.com";
-    const password = "password123";
-    const userId = await insertUser(db, email, password);
-    expect(userId).toBeDefined();
-  });
-
-  it("throws error if the user email is already taken", async () => {
-    const email = "test@gmail.com";
-    const password = "password123";
-    await insertUser(db, email, password);
-    try {
-      await insertUser(db, email, password);
-    } catch (error) {
-      if (error instanceof Error) {
-        expect(error).toBeInstanceOf(Error);
-        expect(error.message).toMatch(/UNIQUE constraint failed/);
-      }
-    }
-  });
-  it("throws an error if password is empty", async () => {
-    const email = "testtest.com";
-    const password = "";
-    try {
-      await insertUser(db, email, password);
-    } catch (error) {
-      if (error instanceof Error) {
-        expect(error).toBeInstanceOf(Error);
-        expect(error.message).toMatch(/password must not be empty/);
-      }
-    }
-  });
-});
-
-describe("getUsersByEmail", () => {
-  it("returns user by email", async () => {
-    const email = "test@test.com";
-    const password = "password123";
-    await insertUser(db, email, password);
-    const user = getUserByEmail(db, email);
-    expect(user).toEqual({
-      id: expect.any(String),
-      password_hash: expect.any(String),
-    });
-  });
-
-  it("returns null if user does not exist", async () => {
-    const email = "test@test.com";
-    const user = getUserByEmail(db, email);
-    expect(user).toBeNull();
-  });
-});
-
-describe("getUsersById", () => {
-  it("returns user by id", async () => {
-    const email = "test@test.com";
-    const password = "password123";
-    const userId = await insertUser(db, email, password);
-    const user = getUserById(db, userId);
-    expect(user).toEqual({
-      id: expect.any(String),
-      email: "test@test.com",
-    });
-  });
-
-  it("returns null if user does not exist", async () => {
-    const user = getUserById(db, "anyId");
-    expect(user).toBeNull();
-  });
-});
-
 describe("insertWorkout", () => {
   it("inserts a workout", async () => {
     const userId = "userId1";
-    const workout = await insertWorkout(db, workoutData, userId);
+    const workout = insertWorkout(db, workoutData, userId);
     expect(workout).toEqual({
       id: expect.any(String),
       name: workoutData.name,
@@ -114,7 +47,7 @@ describe("insertWorkout", () => {
     } as unknown as WorkoutData;
     const userId = "userId1";
     try {
-      await insertWorkout(db, workoutData, userId);
+      insertWorkout(db, workoutData, userId);
     } catch (error) {
       if (error instanceof Error) {
         expect(error).toBeInstanceOf(Error);
