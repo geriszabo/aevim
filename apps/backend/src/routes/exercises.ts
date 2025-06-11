c8ba5a075f289f4766d6fde7ae7783e2ca107c3c
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-06-11 19:56:37 +0200

    fix: correct import path and update insertExercise function in exercises route

--- apps/backend/src/routes/exercises.ts
+++ apps/backend/src/routes/exercises.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { dbConnect } from "../db/db";
-import { exerciseValidator } from "../db/schemas/exercise-shema";
-import { insertExerciseToWorkout } from "../db/queries/exercise-queries";
+import { exerciseValidator } from "../db/schemas/exercise-schema";
+import { insertExercise } from "../db/queries/exercise-queries";
 
 const exercises = new Hono();
 
@@ -10,7 +10,7 @@ exercises.post("/exercises", exerciseValidator, async (c) => {
   const payload = c.get("jwtPayload");
   const { name, category } = c.req.valid("json");
   try {
-    const exercise = insertExerciseToWorkout(db, { name, category }, payload.sub);
+    const exercise = insertExercise(db, { name, category }, payload.sub);
     return c.json({ message: "exercise created successfully", exercise }, 201);
   } catch (error) {
      if (
