29724feed19212efbfb2936306bcbf52026dee05
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-06-19 13:37:40 +0200

    refactor: moved sets endpoints to workouts endpoints

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,8 +4,6 @@ import { jwt } from "hono/jwt";
 import workouts from "./routes/workouts";
 import env from "./env";
 import exercises from "./routes/exercises";
-import sets from "./routes/sets";
-
 const app = new Hono();
 
 app
@@ -13,7 +11,6 @@ app
   .route("api/v1", auth)
   .route("api/v1/auth", workouts)
   .route("api/v1/auth", exercises)
-  .route("api/v1/auth", sets);
 
 export default {
   port: env.PORT || 3000,
--- apps/backend/src/routes/workouts.ts
+++ apps/backend/src/routes/workouts.ts
@@ -18,6 +18,8 @@ import {
   deleteExerciseFromWorkout,
   insertExerciseToWorkout,
 } from "../db/queries/workout-exercises-queries";
+import { setValidator } from "../db/schemas/set-schema";
+import { getAllSetsByExerciseId, insertSet } from "../db/queries/set-queries";
 
 const workouts = new Hono();
 
@@ -191,6 +193,61 @@ workouts
     } catch (error) {
       console.error(error);
       return c.json({ errors: ["Failed to fetch exercises for workout"] }, 500);
+    }
+  })
+  .post(
+    "/workouts/:workoutId/exercises/:exerciseId/sets",
+    setValidator,
+    async (c) => {
+      const db = dbConnect();
+      const workoutId = c.req.param("workoutId");
+      const exerciseId = c.req.param("exerciseId");
+      const payload = c.get("jwtPayload");
+      const setData = c.req.valid("json");
+
+      try {
+        const set = insertSet(db, setData, payload.sub, workoutId, exerciseId);
+        return c.json(
+          {
+            message: "Set added to exercise successfully",
+            set,
+          },
+          201
+        );
+      } catch (error) {
+        if (
+          error instanceof Error &&
+          error.message === "WORKOUT_EXERCISE_NOT_FOUND"
+        ) {
+          return c.json({ errors: ["Workout exercise not found"] }, 404);
+        }
+        console.error(error);
+        return c.json({ errors: ["Failed to add set to exercise"] }, 500);
+      }
+    }
+  )
+  .get("/workouts/:workoutId/exercises/:exerciseId/sets", async (c) => {
+    const db = dbConnect();
+    const workoutId = c.req.param("workoutId");
+    const exerciseId = c.req.param("exerciseId");
+    const payload = c.get("jwtPayload");
+    try {
+      const sets = getAllSetsByExerciseId(
+        db,
+        payload.sub,
+        workoutId,
+        exerciseId
+      );
+
+      return c.json({ sets }, 200);
+    } catch (error) {
+      if (
+        error instanceof Error &&
+        error.message === "WORKOUT_EXERCISE_NOT_FOUND"
+      ) {
+        return c.json({ errors: ["Could not find exercise with set"] }, 404);
+      }
+      return c.json({ errors: ["Internal server error"] }, 500);
     }
   })
   .get("workouts/:id/overview", async (c) => {
