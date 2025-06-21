f86ca05ffe04326b78fe2402bbfeb4f2545293d6
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-06-21 09:06:17 +0200

    chore: upgraded to zod v4 in env.ts

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -1,12 +1,11 @@
-import { z } from "zod";
-import type { ZodError } from "zod/v4";
+import * as z from "zod/v4";
 
 const EnvSchema = z.object({
   JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3000),
   FOREIGN_KEY_CHECKS: z.enum(["ON", "OFF"]),
-  BASE_URL: z.string().url().default("http://localhost:3000"),
-  API_BASE_URL: z.string().url().default("http://localhost:3000/api/v1"),
+  BASE_URL: z.url().default("http://localhost:3000"),
+  API_BASE_URL: z.url().default("http://localhost:3000/api/v1"),
 });
 
 export type Env = z.infer<typeof EnvSchema>;
@@ -15,9 +14,9 @@ let env: Env;
 try {
   env = EnvSchema.parse(process.env);
 } catch (error) {
-  const zodError = error as ZodError 
+  const zodError = error as z.ZodError 
   console.error("THE SHRIMP OF INVALID .ENV FILE HAS VISITE YOU✨🦐✨")
-  console.error(JSON.stringify(zodError.flatten().fieldErrors, null, 2));
+  console.error(z.prettifyError(zodError))
   process.exit(1);
 }
 
