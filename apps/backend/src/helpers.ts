1596dd53d3d9892c8b39941ba5fe8251d9570885
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-06-21 12:10:43 +0200

    chore: created error handler for endpoints

--- apps/backend/src/helpers.ts
+++ apps/backend/src/helpers.ts
@@ -2,6 +2,9 @@ import { Database } from "bun:sqlite";
 import { sign } from "hono/jwt";
 import type { CookieOptions } from "hono/utils/cookie";
 import env from "./env";
+import type { Context } from "hono";
+
+type ErrorCode = keyof typeof ERROR_MAPPINGS;
 
 export const createTable = (
   dbInstance: Database,
@@ -35,4 +38,32 @@ export const cookieOptions: CookieOptions = {
   sameSite: "Lax", // or Strict
   path: "/",
   maxAge: 3600, //1 hour
+};
+
+const ERROR_MAPPINGS = {
+  EXERCISE_NOT_FOUND: { status: 404, message: "Exercise not found" },
+  WORKOUT_NOT_FOUND: { status: 404, message: "Workout not found" },
+  SET_NOT_FOUND: { status: 404, message: "Set not found" },
+  NO_EXERCISES_FOUND: { status: 404, message: "No exercises found" },
+  WORKOUT_EXERCISE_NOT_FOUND: {
+    status: 404,
+    message: "Workout exercise not found",
+  },
+} as const;
+
+export const handleError = (
+  c: Context,
+  error: unknown,
+  fallbackMessage = "Internal server error"
+) => {
+  if (error instanceof Error) {
+    const errorCode = error.message as ErrorCode;
+
+    if (ERROR_MAPPINGS[errorCode]) {
+      const { status, message } = ERROR_MAPPINGS[errorCode];
+      return c.json({ errors: [message] }, status);
+    }
+  }
+
+  return c.json({ errors: [fallbackMessage] }, 500);
 };
