26ed49af30cf3cd6cea96192cea6fca7e6ef6879
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-06-21 08:08:45 +0200

    chore: added base url to zod env schema

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -4,6 +4,8 @@ const EnvSchema = z.object({
   JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3000),
   FOREIGN_KEY_CHECKS: z.enum(["ON", "OFF"]),
+  BASE_URL: z.string().url().default("http://localhost:3000"),
+  API_BASE_URL: z.string().url().default("http://localhost:3000/api/v1"),
 });
 
 const env = EnvSchema.parse(process.env);
