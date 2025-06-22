d86fed200be61a35202a916b07e45d12c8c953de
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-06-22 17:24:51 +0200

    feat: add CORS middleware to API routes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,9 +4,11 @@ import { jwt } from "hono/jwt";
 import workouts from "./routes/workouts";
 import env from "./env";
 import exercises from "./routes/exercises";
+import { cors } from "hono/cors";
 const app = new Hono();
 
 app
+  .use("/api/*", cors())
   .use("api/v1/auth/*", jwt({ secret: env.JWT_SECRET, cookie: "authToken" }))
   .route("api/v1", auth)
   .route("api/v1/auth", workouts)
