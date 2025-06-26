2bf4d8073d1c157c16c0ec18f9168b70673e95d9
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-06-26 19:37:08 +0200

    feat: created FormInputField comp

---
+++ apps/frontend/src/components/Form/FormInputField.tsx
@@ -0,0 +1,54 @@
+import { Label } from "@radix-ui/react-label";
+import { ComponentProps, ReactNode } from "react";
+import { Input } from "../ui/input";
+import { FieldError, FieldValues, Path, UseFormRegister } from "react-hook-form";
+
+interface FormInputFieldProps<T extends FieldValues> {
+  id: Path<T>;
+  label: string;
+  icon?: ReactNode;
+  register: UseFormRegister<T>;
+  type: ComponentProps<"input">["type"];
+  placeholder?: string;
+  autoComplete?: ComponentProps<"input">["autoComplete"];
+  autoFocus?: boolean;
+  className?: ComponentProps<"input">["className"];
+  error?: FieldError;
+}
+
+export const FormInputField = <T extends FieldValues>({
+  label,
+  icon,
+  id,
+  type,
+  register,
+  placeholder,
+  autoComplete,
+  autoFocus = false,
+  error,
+}: FormInputFieldProps<T>) => {
+  return (
+    <div className="space-y-2">
+      <Label htmlFor={id} className="text-sm font-medium">
+        {label}
+      </Label>
+      <div className="relative">
+        {icon && (
+          <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+            {icon}
+          </div>
+        )}
+        <Input
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          className={`${icon ? "pl-10" : ""} h-12 `}
+          autoComplete={autoComplete}
+          autoFocus={autoFocus}
+          {...register(id)}
+        />
+      </div>
+      {<p className="text-sm text-red-500">{error?.message}</p>}
+    </div>
+  );
+};
