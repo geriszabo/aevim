dc723f7f584a15768ee063e5074d5900a99a5efb
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-06-27 22:44:38 +0200

    feat: implement postLogin function

---
+++ apps/frontend/src/hooks/api/postLogin.ts
@@ -0,0 +1,17 @@
+import env from "@/env";
+import { LoginFormData } from "@/schemas/login-schema";
+import { API_ROUTES } from "@aevim/shared-types/api-routes";
+
+export const postLogin = async ({
+  email,
+  password,
+}: LoginFormData) => {
+  return await fetch(`${env.API_BASE_URL}${API_ROUTES.auth.login}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  
+};
