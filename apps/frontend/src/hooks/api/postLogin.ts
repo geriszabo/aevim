5ec89316b04df165a39e02ae5957537df550e7a9
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-06-29 20:40:30 +0200

    fix: add credentials in auth requests

--- apps/frontend/src/hooks/api/postLogin.ts
+++ apps/frontend/src/hooks/api/postLogin.ts
@@ -2,16 +2,13 @@ import env from "@/env";
 import { LoginFormData } from "@/schemas/login-schema";
 import { API_ROUTES } from "@aevim/shared-types/api-routes";
 
-export const postLogin = async ({
-  email,
-  password,
-}: LoginFormData) => {
+export const postLogin = async ({ email, password }: LoginFormData) => {
   return await fetch(`${env.API_BASE_URL}${API_ROUTES.auth.login}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
     body: JSON.stringify({ email, password }),
   });
-  
 };
--- apps/frontend/src/hooks/api/postSignup.ts
+++ apps/frontend/src/hooks/api/postSignup.ts
@@ -2,16 +2,13 @@ import env from "@/env";
 import { LoginFormData } from "@/schemas/login-schema";
 import { API_ROUTES } from "@aevim/shared-types/api-routes";
 
-export const postSignup = async ({
-  email,
-  password,
-}: LoginFormData) => {
+export const postSignup = async ({ email, password }: LoginFormData) => {
   return await fetch(`${env.API_BASE_URL}${API_ROUTES.auth.signup}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
     body: JSON.stringify({ email, password }),
   });
-  
 };
