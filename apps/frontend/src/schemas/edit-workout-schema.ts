5d0d3386882ec395ed21b97c012a62637a4b63c2
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-07-05 23:52:05 +0200

    feat: add editWorkoutSchema for validating workout edit data

---
+++ apps/frontend/src/schemas/edit-workout-schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod/v4";
+
+export const editWorkoutSchema = z.object({
+  name: z.string({ message: "No string for name update provided" }).optional(),
+  date: z.iso
+    .date({ message: "No string for date update provided" })
+    .optional(),
+  notes: z
+    .string({ message: "No string for notes update provided" })
+    .optional(),
+});
+
+export type EditWorkoutData = z.infer<typeof editWorkoutSchema>;
