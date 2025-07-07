3a0ac0008e5848739c933537cd961ef3c0ef3a00
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-07-07 21:55:23 +0200

    chore: created exercise schema

---
+++ apps/frontend/src/schemas/create-exercise-schema.ts
@@ -0,0 +1,8 @@
+import z from "zod/v4";
+
+export const createExerciseSchema = z.object({
+  name: z.string({ message: "You have to give the exercise a name" }),
+  category: z.string().nullable().optional(),
+});
+
+export type CreateExerciseData = z.infer<typeof createExerciseSchema>;
