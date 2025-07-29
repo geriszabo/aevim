0252ba8fb562c835acb11174ef72edecfcc140f2
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-07-29 17:33:58 +0200

    fix: updated zod versions

--- apps/backend/src/types/exercise.ts
+++ apps/backend/src/types/exercise.ts
@@ -1,6 +1,6 @@
 import type { Exercise } from "@aevim/shared-types";
+import type z from "zod/v4";
 import type { exerciseSchema } from "../db/schemas/exercise-schema";
-import type z from "zod";
 
 export type ExerciseData = z.infer<typeof exerciseSchema>;
 
