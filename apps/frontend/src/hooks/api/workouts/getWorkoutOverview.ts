42017fe37a406697620ab82da0cb328f061688c3
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-09-27 17:25:35 +0200

    refactor: enhance getWorkoutOverview to handle optional cookie parameter

--- apps/frontend/src/hooks/api/workouts/getWorkoutOverview.ts
+++ apps/frontend/src/hooks/api/workouts/getWorkoutOverview.ts
@@ -1,21 +1,31 @@
 import env from "@/env";
+import { GetWorkoutOverviewResponse } from "@/types/api";
 import { handleApiError } from "@/utils/handleApiError";
 import { API_ROUTES } from "@aevim/shared-types";
 
-
-export const getWorkoutOverview = async (workoutId: string) => {
+export const getWorkoutOverview = async (
+  workoutId: string,
+  cookie?: string
+): Promise<GetWorkoutOverviewResponse> => {
   const response = await fetch(
     `${env.API_BASE_URL}${API_ROUTES.completeWorkouts.single(workoutId)}`,
     {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
+        ...(cookie && { Cookie: cookie }),
       },
       credentials: "include",
     }
   );
+
   if (!response.ok) {
-    handleApiError(response);
+    if (cookie) {
+      throw new Error(`Failed to fetch workout: ${response.status}`);
+    } else {
+      handleApiError(response);
+    }
   }
+
   return response.json();
-}
+};
