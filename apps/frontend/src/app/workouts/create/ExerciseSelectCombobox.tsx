a585cd1dc732f39dce86db164994fb61002e5e50
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-10-12 18:56:21 +0200

    chore: created combobox for selecting exercises

---
+++ apps/frontend/src/app/workouts/create/ExerciseSelectCombobox.tsx
@@ -0,0 +1,84 @@
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import exercises from "@aevim/shared-types/exercises.json";
+import { Label } from "@/components/ui/label";
+
+interface ExerciseSelectComboBoxProps {
+  value?: string;
+  onChangeValue: (value: string) => void;
+  
+}
+
+const exerciseList = exercises.map((ex) => ({ id: ex.id, label: ex.name }));
+
+
+export const ExerciseSelectComboBox = ({
+  onChangeValue,
+  value,
+}: ExerciseSelectComboBoxProps) => {
+  const [open, setOpen] = useState(false);
+  
+  return (
+    <>
+      <Label>Exercise name</Label>
+      <Popover open={open} onOpenChange={setOpen} modal={true}>
+        <PopoverTrigger asChild className="w-full">
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-full justify-between h-10"
+          >
+            {value
+              ? exerciseList.find((ex) => ex.label === value)?.label
+              : "Select exercise..."}
+            <ChevronsUpDown className="opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-full p-0">
+          <Command>
+            <CommandInput placeholder="Search workout..." className="h-9" />
+            <CommandList>
+              <CommandEmpty>No workout found.</CommandEmpty>
+              <CommandGroup>
+                {exerciseList.map((ex) => (
+                  <CommandItem
+                    key={ex.id}
+                    value={ex.label}
+                    onSelect={() => {
+                      onChangeValue(ex.label === value ? "" : ex.label);
+                      setOpen(false);
+                    }}
+                  >
+                    {ex.label}
+                    <Check
+                      className={cn(
+                        "ml-auto",
+                        value === ex.label ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
+  );
+};
