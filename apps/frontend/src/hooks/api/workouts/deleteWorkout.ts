aec7de5a4390764094b1833cf570734a73a8c3d7
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-10-25 11:08:16 +0200

    refactor: moved delete workout to completeWorkouts routes

--- apps/frontend/src/hooks/api/workouts/deleteWorkout.ts
+++ apps/frontend/src/hooks/api/workouts/deleteWorkout.ts
@@ -4,7 +4,7 @@ import { API_ROUTES } from "@aevim/shared-types";
 
 export const deleteWorkout = async (workoutId: string) => {
   const response = await fetch(
-    `${env.API_BASE_URL}${API_ROUTES.workouts.single(workoutId)}`,
+    `${env.API_BASE_URL}${API_ROUTES.completeWorkouts.single(workoutId)}`,
     {
       method: "DELETE",
       headers: {
