5027a5c4d5e54f9d5330b8d423296ae0d6d36b11
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-11-01 17:57:14 +0100

    chore: added some utils

--- apps/frontend/src/lib/utils.ts
+++ apps/frontend/src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { SetMetrics } from "@aevim/shared-types";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getUnit = (metricType: SetMetrics | undefined) => {
+  switch (metricType) {
+    case "distance":
+      return "km";
+    case "duration":
+      return "min";
+    case "weight":
+      return "kg";
+    default:
+      return "";
+  }
+};
+
+export const toSentenceCase = (str: string | undefined): string => {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
