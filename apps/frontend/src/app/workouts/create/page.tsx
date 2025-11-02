7a19a6fceab9105843c6480a8e8a448c430ed6f7
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-11-02 17:35:37 +0100

    chore: added default value for workout name

--- apps/frontend/src/app/workouts/create/page.tsx
+++ apps/frontend/src/app/workouts/create/page.tsx
@@ -2,12 +2,15 @@
 
 import { WorkoutForm } from "@/components/Form/WorkoutForm";
 import { useCreateCompleteWorkout } from "@/hooks/workouts/useCreateCompleteWorkout";
+import { format } from "date-fns";
 
 export default function WorkoutPage() {
   const { mutate } = useCreateCompleteWorkout();
+
+  const workoutDefaultName = `${format(new Date(), "cccc", )} workout / ${format(new Date(), "do MMMM")}`
   const defaultValues = {
     workout: {
-      name: "",
+      name: workoutDefaultName,
       date: new Date().toISOString().split("T")[0],
       notes: "",
     },
