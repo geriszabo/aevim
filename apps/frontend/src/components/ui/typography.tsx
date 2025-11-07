d204278c894fdb6dff6b856fdf58b73818128d35
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-11-07 17:05:37 +0100

    refactor: refactored Typography component

--- apps/frontend/src/components/ui/typography.tsx
+++ apps/frontend/src/components/ui/typography.tsx
@@ -1,11 +1,9 @@
 import { TypographySize } from "@/types/utils";
+import { HTMLAttributes } from "react";
 
-interface TypographyProps {
+interface TypographyProps extends HTMLAttributes<HTMLParagraphElement> {
   variant: "hero" | "heading" | "body" | "muted";
   size?: TypographySize;
-  children: React.ReactNode;
-  className?: string;
-  onClick?: () => void;
 }
 
 export const Typography = ({
@@ -13,12 +11,12 @@ export const Typography = ({
   size,
   children,
   className,
-  onClick,
+  ...props
 }: TypographyProps) => {
   if (variant === "hero") {
     return (
       <h1
-        onClick={onClick}
+        {...props}
         className={`${
           size ? `text-${size}` : "text-4xl"
         } font-bold font-heading leading-tight bg-gradient-to-r from-gray-900 to-slate-600 dark:from-slate-100 dark:to-slate-400 bg-clip-text text-transparent ${className}`}
@@ -31,7 +29,7 @@ export const Typography = ({
   if (variant === "heading") {
     return (
       <h2
-        onClick={onClick}
+        {...props}
         className={`${
           size ? `text-${size}` : "text-3xl"
         } font-bold font-heading leading-tight ${className}`}
@@ -43,7 +41,7 @@ export const Typography = ({
   if (variant === "muted") {
     return (
       <p
-        onClick={onClick}
+        {...props}
         className={`${
           size ? `text-${size}` : "text-base"
         } text-muted-foreground ${className}`}
@@ -55,7 +53,7 @@ export const Typography = ({
 
   return (
     <p
-      onClick={onClick}
+      {...props}
       className={`${size ? `text-${size}` : "text-base"} ${className}`}
     >
       {children}
