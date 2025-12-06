93a118446470222e8fc4805f82030604e82967e8
Gergö Szabo <gergo.szabo@marshmallow.co>
2025-12-06 17:43:06 +0100

    refactor: converted RecentWorkoutsCard.tsx to SSR

--- apps/frontend/src/app/dashboard/RecentWorkoutsCard.tsx
+++ apps/frontend/src/app/dashboard/RecentWorkoutsCard.tsx
@@ -1,41 +1,40 @@
-import { Button } from "@/components/ui/button";
+"use server";
+
 import { Card } from "@/components/ui/card";
 import { Typography } from "@/components/ui/typography";
 import { Calendar, Dumbbell, TrendingUp } from "lucide-react";
-import { useRouter } from "next/navigation";
+
 import { type CompleteWorkout } from "@aevim/shared-types";
+import Link from "next/link";
 
-export const RecentWorkoutsCard = (workout: CompleteWorkout["workout"]) => {
-  const router = useRouter();
-
+export const RecentWorkoutsCard = async (
+  workout: CompleteWorkout["workout"],
+) => {
   return (
-    <Card
-      key={workout.id}
-      className="p-2"
-      onClick={() => router.push(`/workouts/${workout.id}`)}
-    >
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <div className="h-10 w-10 bg-decor rounded-lg flex items-center justify-center">
-            <Dumbbell className="h-5 w-5 text-green-500" />
-          </div>
-          <div>
-            <Typography variant="body" size="md" className="mb-1">
-              {workout.name}
-            </Typography>
-            <div className="flex items-center gap-4 text-sm text-muted-foreground">
-              <div className="flex items-center gap-1">
-                <Calendar className="h-3 w-3" />
-                <span>{workout.date}</span>
+    <Link href={`/workouts/${workout.id}`} className="block">
+      <Card key={workout.id} className="p-2">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <div className="h-10 w-10 bg-decor rounded-lg flex items-center justify-center">
+              <Dumbbell className="h-5 w-5 text-green-500" />
+            </div>
+            <div>
+              <Typography variant="body" size="md" className="mb-1">
+                {workout.name}
+              </Typography>
+              <div className="flex items-center gap-4 text-sm text-muted-foreground">
+                <div className="flex items-center gap-1">
+                  <Calendar className="h-3 w-3" />
+                  <span>{workout.date}</span>
+                </div>
               </div>
             </div>
           </div>
+          <div className="h-10 w-10 flex items-center justify-center">
+            <TrendingUp className="h-4 w-4 text-decor " />
+          </div>
         </div>
-
-        <Button variant="ghost" size="icon" className="h-10 w-10">
-          <TrendingUp className="h-4 w-4 text-decor " />
-        </Button>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   );
 };
